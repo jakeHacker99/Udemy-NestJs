3bb7e1cd750bf9cff5fb63fa02f9ab532d679c2d
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-08-18 12:51:36 +0200

    Realtions with TypeORM

--- my-cv/test/auth.e2e-spec.ts
+++ my-cv/test/auth.e2e-spec.ts
@@ -28,20 +28,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
-  it("should signup and get user", async () => {
-    const email = "shushu124@gmail.com"
 
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
 
     const res = await request(app.getHttpServer())
-      .post("/auth/signup")
-      .send({ email, password: "124" })
-      .expect(201)
-    const cookie = res.get("Set-Cookie")
-    const { body } = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
 
-      .get("/auth/whoami")
-      .set("Cookie", cookie)
-      .expect(200)
-    expect(body.email).toEqual(email)
-  })
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
